9423bb69c8cac8eb5ed9c6c9924d3447b14aec22
maydinn <muhammedaydin9214@gmail.com>
2024-05-03 21:01:17 +0200

    all

--- Info.py
+++ Info.py
@@ -44,9 +44,3 @@ st.write("""
 In der heutigen global vernetzten Welt ist die Nutzung von Daten zu einem integralen Bestandteil unseres täglichen Lebens geworden und dringt in nahezu jeden Aspekt vor. Wirtschaftliche Indikatoren spielen dabei eine herausragende Rolle und sind fester Bestandteil unserer Alltagsroutinen geworden. Gespräche über Themen wie Inflation, wirtschaftliche Stabilität und globale Wirtschaftstrends sind mittlerweile alltäglich geworden. Ein häufiges Problem besteht jedoch darin, dass die Indikatoren, mit denen wir tagtäglich konfrontiert sind, oft zu allgemein gehalten sind. Wir hören vielleicht von globalen Phänomenen wie steigenden Temperaturen oder sich ändernden Wettermustern, aber es fehlt uns oft ein klares Verständnis dafür, was in unserer unmittelbaren Umgebung geschieht. Diese Webseite untersucht die Bedeutung der Präsentation von wirtschaftlichen und wetterbezogenen Indikatoren auf lokaler Ebene, am Beispiel der deutschen Bundesländer, und stellt Erklärungen bereit, um Bürgerinnen und Bürgern wertvolle Einblicke zu vermitteln.
 """)
 
-with open('visits.csv', 'a+') as f:    #Append & read mode
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    f.write(f"{timestamp}\n")
-    #st.write(timestamp)
-data = pd.read_csv('visits.csv')
-st.metric("", '','','normal',  f'{data.time.values[0]}, {len(data)}')
